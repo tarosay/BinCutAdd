26980c2cae8ab6a94229c0eb53f6dbfa26a17d0c
tarosay <tarosa.y@gmail.com>
2016-09-01 14:28:51 +0900

    First Commit

---
+++ BinCutAdd/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BinCutAdd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxMes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxAname0 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 19);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\tmp\\";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "分　割";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "分割するファイル名";
+            // 
+            // tbxMes
+            // 
+            this.tbxMes.Location = new System.Drawing.Point(335, 15);
+            this.tbxMes.Multiline = true;
+            this.tbxMes.Name = "tbxMes";
+            this.tbxMes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxMes.Size = new System.Drawing.Size(411, 322);
+            this.tbxMes.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "結合するファイル名(番号無しで)";
+            // 
+            // tbxAname0
+            // 
+            this.tbxAname0.Location = new System.Drawing.Point(14, 85);
+            this.tbxAname0.Name = "tbxAname0";
+            this.tbxAname0.Size = new System.Drawing.Size(234, 19);
+            this.tbxAname0.TabIndex = 5;
+            this.tbxAname0.Text = "C:\\tmp\\";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(38, 19);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "結　合";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "ファイル総数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 346);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tbxAname0);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxMes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxMes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxAname0;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BinCutAdd/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinCutAdd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string filename = textBox1.Text;
+
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(fs);
+
+            int i = 0;
+            long a = 1;
+            while (a > 0)
+            {
+                byte[] b = r.ReadBytes(250000000);
+                //byte[] b = r.ReadBytes(500000000);
+                //byte[] b = r.ReadBytes(1000000000);
+                a = (long)b.Length;
+                if (a <= 0)
+                {
+                    break;
+                }
+                Debug.WriteLine(b.Length.ToString() + "バイト読み込みました");
+                tbxMes.Text += b.Length.ToString() + "バイト読み込みました\r\n";
+                GC.Collect();
+                
+                tbxMes.SelectionStart = tbxMes.Text.Length;
+                //テキストボックスにフォーカスを移動
+                tbxMes.Focus();
+                //カレット位置までスクロール
+                tbxMes.ScrollToCaret();
+
+                Application.DoEvents();
+
+
+                FileStream fw = new FileStream(filename + "." + i.ToString("00"), FileMode.Create);
+                BinaryWriter w = new BinaryWriter(fw);
+
+                w.Write(b);
+                fw.Close();
+                Debug.WriteLine(b.Length.ToString() + "切り出しました");
+                tbxMes.Text += b.Length.ToString() + "バイト切り出しました\r\n";
+                GC.Collect();
+                
+                tbxMes.SelectionStart = tbxMes.Text.Length;
+                //テキストボックスにフォーカスを移動
+                tbxMes.Focus();
+                //カレット位置までスクロール
+                tbxMes.ScrollToCaret();
+                
+                Application.DoEvents();
+
+                b = null;
+                GC.Collect();
+                GC.Collect();
+                i++;
+            }
+            fs.Close();
+
+            Debug.WriteLine("切り出しを終了しました");
+            tbxMes.Text += "切り出しを終了しました\r\n";
+
+            tbxMes.SelectionStart = tbxMes.Text.Length;
+            //テキストボックスにフォーカスを移動
+            tbxMes.Focus();
+            //カレット位置までスクロール
+            tbxMes.ScrollToCaret();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filename = tbxAname0.Text;
+            int s = int.Parse(textBox3.Text);
+            string rfname = "";
+
+            //FileStream fw = new FileStream(Path.GetDirectoryName(filename) + "\\" + Path.GetFileNameWithoutExtension(filename), FileMode.Create);
+            FileStream fw = new FileStream(filename, FileMode.Create);
+            BinaryWriter w = new BinaryWriter(fw);
+
+            for (int i = 0; i < s; i++)
+            {
+                //rfname = Path.GetDirectoryName(filename) + "\\" + Path.GetFileNameWithoutExtension(filename) + "." + i.ToString("00");
+                rfname = filename + "." + i.ToString("00");
+                FileStream fs = new FileStream(rfname, FileMode.Open, FileAccess.Read);
+                BinaryReader r = new BinaryReader(fs);
+
+                byte[] b = r.ReadBytes(250000000);
+
+                fs.Close();
+
+                //        Debug.WriteLine(b.Length.ToString() + "バイト読み込みました");
+                tbxMes.Text += rfname + "から、" + b.Length.ToString() + "バイト読み込みました\r\n";
+                GC.Collect();
+                
+                tbxMes.SelectionStart = tbxMes.Text.Length;
+                //テキストボックスにフォーカスを移動
+                tbxMes.Focus();
+                //カレット位置までスクロール
+                tbxMes.ScrollToCaret();
+
+                Application.DoEvents();
+
+                w.Write(b);
+
+                tbxMes.Text += b.Length.ToString() + "バイト書き足しました\r\n";
+                GC.Collect();
+
+                tbxMes.SelectionStart = tbxMes.Text.Length;
+                //テキストボックスにフォーカスを移動
+                tbxMes.Focus();
+                //カレット位置までスクロール
+                tbxMes.ScrollToCaret();
+
+                Application.DoEvents();
+
+                b = null;
+                GC.Collect();
+                GC.Collect();
+            }
+            fw.Close();
+            
+            //Debug.WriteLine("切り出しを終了しました");
+            tbxMes.Text += "結合を終了しました\r\n";
+
+            tbxMes.SelectionStart = tbxMes.Text.Length;
+            //テキストボックスにフォーカスを移動
+            tbxMes.Focus();
+            //カレット位置までスクロール
+            tbxMes.ScrollToCaret();
+
+        }
+    }
+}
